03fec5b40db8ff52c608b00415ecd46215f5bff1
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-01-11 23:10:10 +0600

    Fix social account login system

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
   path('', views.hello_view, name='home'),
   path ('signup', views.donor_signup, name='donor_signup'),
-  path('accounts/login/', LoginView.as_view(), name='login'),
+  path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import render
 from .forms import DonorSignUpForm
+from allauth.account.views import LoginView
+from allauth.account.forms import LoginForm
 
 # Create your views here.
 def hello_view(request):
@@ -17,3 +19,19 @@ def donor_signup(request):
   else:
     form = DonorSignUpForm()
   return render(request, 'donor_signup.html', {'form' : form})
+
+class CustomLoginView(LoginView):
+  form_class = LoginForm
+  print('I am here at main view')
+
+  def form_invalid(self, form):
+    print('I am here at the form')
+    response = super().form_invalid(form)
+
+    username_error = form.errors.get('username', None)
+    password_error = form.errors.get('password', None)
+
+    response.context_data['username_error'] = username_error
+    response.context_data['password_error'] = password_error
+
+    return response
